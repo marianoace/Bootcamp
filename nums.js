8eb54fe17a1ad65ca0d8ea4283303baa90b13587
marianoace <78988663+marianoace@users.noreply.github.com>
2023-03-27 15:15:50 -0400

    Add files via upload

---
+++ nums.js
@@ -0,0 +1,8 @@
+let HeightCms = 175;
+let HeightMts = 1.75;
+let WeightKgs = 71.7;
+let HeightMtsRounded= Math.ceil(HeightMts);
+let WeightKgsRounded = Math.floor(WeightKgs);
+let maxNum = Number.MAX_VALUE;
+let maxNumPlusOne= Number.MAX_VALUE + 1;
+let itsEqual = console.log(maxNum === maxNumPlusOne)
